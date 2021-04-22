db487435aa12d84726d5bda6fb6d96d29058499b
skymins04 <skymin0417@gmail.com>
2021-04-22 17:56:45 +0900

    중복 업체 필터링 기능 추가

--- README.md
+++ README.md
@@ -11,12 +11,49 @@ G마켓의 베스트 제품별 셀러 정보를 랭킹순으로 크롤링하는 
 
 ## 사용법
 1. 텍스트 편집기를 이용해 preset.json 파일 내의 정보 기입
+
+    + MODE - 크롤링 모드를 설정함 (필수 필드, 타입: string)
+        + "default": G마켓 기본 브라우징 제품 내에서 크롤링 
+        + "best": G마켓 베스트 제품 내에서 크롤링
+
+    + SELLERS - 크롤링하려는 셀러의 수 (필수 필드, 타입: number, MODE-best의 경우 범위: 0~80, MODE-default의 경우 범위: 0~무제한)
+
+    + OUTPUTDIR - 크롤링 데이터의 출력 위치 (필수 필드, 타입: string, 디렉토리 형태의 문자열)
+
+    + FILENAME - 출력 데이터의 접두어 (필수 필드, 타입: string)
+
+    + LARGE_CATEGORY_CODE - G마켓 최상위 카테고리의 코드 기입 (필수 필드, 타입: string)
+
+    + MEDIUM_CATEGORY_CODE - G마켓 중간 카테고리의 코드 기입 (타입: string)
+
+    + SMALL_CATEGORY_CODE - G마켓 최하위 카테고리 (타입: string)
+
+    + ONLY_POWERDEALER - G마켓의 파워딜러 등급의 셀러만 크롤링하는 옵션 (기본값: false, 타입: boolean)
 ```
-    (preset.json)
+    (preset.json, mode: default)
 
     {
-        URL: (G마켓의 카테고리별 베스트 상품 리스트 페이지의 주소, type: string),
-        SELLERS: (크롤링하려는 셀러의 수, type: number)
+        "MODE": "default",
+        "LARGE_CATEGORY_CODE": (essential, type: string),
+        "SELLERS": (essential, type: number)
+        "ONLY_POWERDEALER": (optional, type: boolean),
+        "OUTPUTDIR": "./data",
+        "FILENAME": "crawling_data"
+    }
+
+```
+```
+    (preset.json, mode: best)
+
+    {
+        "MODE": "best",
+        "LARGE_CATEGORY_CODE": (essential, type: string),
+        "MEDIUM_CATEGORY_CODE" (optional, type: string),
+        "SMALL_CATEGORY_CODE": (optional, type: string),
+        "SELLERS": (essential, type: number),
+        "ONLY_POWERDEALER": (optional, type: boolean),
+        "OUTPUTDIR": "./data",
+        "FILENAME": "crawling_data"
     }
 
 ```
--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const SELLERS = (presetFile.SELLERS <= 0 ?
     )
 );
 const ONLY_POWERDEALER = presetFile.ONLY_POWERDEALER;
+const OUTPUTDIR = presetFile.OUTPUTDIR;
+const FILENAME = presetFile.FILENAME;
 var URL = '';
+var sellers_li = [''];
 
 const makeFolder = (dir) => {
     if (!fs.existsSync(dir)) {
@@ -28,59 +31,91 @@ const getHtml = async (url_) => {
     }
 };
 
-if(MODE === 'default') {
-    throw 'default mode is not supported yet';
-}
-else if(MODE === 'best') {
+if ((!(SELLERS >= 0 && SELLERS <= 80 && SELLERS % 1 === 0) && MODE === 'best') || SELLERS < 0) throw 'SELLERS of preset.json is not vaild.';
+if ((ONLY_POWERDEALER || ONLY_POWERDEALER === false) && typeof ONLY_POWERDEALER !== 'boolean') throw 'ONLY_POWERDEALER of preset.json is not vaild.';
+if (!OUTPUTDIR || typeof OUTPUTDIR !== 'string') throw 'OUTPUTDIR of preset.json is not vaild';
+if (!FILENAME || typeof FILENAME !== 'string') throw 'FILENAME of preset.json is not vaild';
+if (MODE === 'default') {
+    if (!presetFile.LARGE_CATEGORY_CODE) throw 'can not found LARGE_CATEGORY_CODE in preset.json';
+    if (typeof presetFile.LARGE_CATEGORY_CODE !== 'string') throw 'LARGE_CATEGORY_CODE\'s datatype is not \'string\' in preset.json';
+    URL += `http://browse.gmarket.co.kr/list?category=${presetFile.LARGE_CATEGORY_CODE}`;
+} else if (MODE === 'best') {
     URL += 'http://corners.gmarket.co.kr/Bestsellers?viewType=C';
-    if(presetFile.LARGE_CATEGORY_CODE) {
-        if(typeof presetFile.LARGE_CATEGORY_CODE !== 'string') throw 'LARGE_CATEGORY_CODE\'s datatype is not \'string\' in preset.json';
+    if (presetFile.LARGE_CATEGORY_CODE) {
+        if (typeof presetFile.LARGE_CATEGORY_CODE !== 'string') throw 'LARGE_CATEGORY_CODE\'s datatype is not \'string\' in preset.json';
         URL += `&largeCategoryCode=${presetFile.LARGE_CATEGORY_CODE}`;
-        if(presetFile.MEDIUM_CATEGORY_CODE) {
-            if(typeof presetFile.MEDIUM_CATEGORY_CODE !== 'string') throw 'MEDIUM_CATEGORY_CODE\'s datatype is not \'string\' in preset.json';
+        if (presetFile.MEDIUM_CATEGORY_CODE) {
+            if (typeof presetFile.MEDIUM_CATEGORY_CODE !== 'string') throw 'MEDIUM_CATEGORY_CODE\'s datatype is not \'string\' in preset.json';
             URL += `&mediumCategoryCode=${presetFile.MEDIUM_CATEGORY_CODE}`;
-            if(presetFile.SMALL_CATEGORY_CODE) {
-                if(typeof presetFile.SMALL_CATEGORY_CODE !== 'string') throw 'SMALL_CATEGORY_CODE\'s datatype is not \'string\' in preset.json';
+            if (presetFile.SMALL_CATEGORY_CODE) {
+                if (typeof presetFile.SMALL_CATEGORY_CODE !== 'string') throw 'SMALL_CATEGORY_CODE\'s datatype is not \'string\' in preset.json';
                 URL += `&smallCategoryCode=${presetFile.SMALL_CATEGORY_CODE}`;
             }
         }
-    }
-    else throw 'can not found LARGE_CATEGORY_CODE in preset.json';
-}
-else throw 'MODE of preset.json is not vaild';
-
-if(!(SELLERS >= 0 && SELLERS <= 80 && SELLERS % 1 === 0 )) throw 'SELLERS of preset.json is not vaild.';
-if(typeof ONLY_POWERDEALER !== 'boolean') throw 'ONLY_POWERDEALER of preset.json is not vaild.';
+    } else throw 'can not found LARGE_CATEGORY_CODE in preset.json';
+} else throw 'MODE of preset.json is not vaild';
 
 console.log(`crawling mode: ${MODE}`);
 console.log(`crawling sellers: ${SELLERS}`);
 console.log(`only powerdealer: ${ONLY_POWERDEALER}`);
 console.log(`crawling url: ${URL}`);
 
-getHtml(URL).then(html => {
+getHtml(URL).then(async html => {
         let list = [];
-        const $ = cheerio.load(html.data);
-        const $bodyList = $('div.best-list ul li');
+        let page = 1;
+        let url = URL;
 
-        console.log('please waiting for crawling datas...');
+        const $ = cheerio.load(html.data);
+        const $bodyList = $(MODE === 'default' ? 'div.box__component-itemcard--general' : 'div.best-list ul li');
+
+        console.log('finding for datas...');
 
         $bodyList.each(function (i, elem) {
-            list[i] = $(elem).find('a.itemname').attr('href');
+            if(MODE === 'default') {
+                list[i] = `http://${$(elem).find('a.link__itemcard').attr('href').substr(8)}`;
+            }
+            else {
+                list[i] = $(elem).find('a.itemname').attr('href');
+            }
         });
-        console.log(list);
 
-        return list.filter(n => n);
+        list = list.filter(n => n);
+
+        while (list.length < SELLERS*2 && MODE === 'default') {
+            console.log('loading more datas...');
+            page += 1;
+            URL = url + `&p=${page}`;
+            list.push(...await getHtml(URL).then(html => {
+                let _l = [];
+                const $ = cheerio.load(html.data);
+                fs.writeFileSync('./log/log.html', html.data);
+                const $bodyList = $('div.box__component-itemcard--general');
+                $bodyList.each(function (i, elem) {
+                    _l[i] = `http://${$(elem).find('a.link__itemcard').attr('href').substr(8)}`;
+                });
+                _l = _l.filter(n => n);
+                return _l;
+            }));
+        }
+
+        list = list.filter(n => n);
+
+        console.log(list);
+        console.log(`found datas... ${list.length}`);
+
+        return list;
     })
     .then(async urls => {
         let seller_list = [];
         let ext_idx = 0;
 
+        console.log('please waiting for crawling datas...');
+
         for (i = 0; i < ((urls.length >= SELLERS + ext_idx) ? SELLERS + ext_idx : urls.length); i++) {
             seller_list.push(await getHtml(urls[i]).then(async html => {
                 console.log(`crawling ${i}: ${urls[i]}`);
-                fs.writeFileSync('./log/log.html', html.data);
-                
-                var substring_data = html.data.substring(html.data.indexOf('var goods = ')+12, html.data.length);
+
+                var substring_data = html.data.substring(html.data.indexOf('var goods = ') + 12, html.data.length);
                 console.log(substring_data.substring(0, substring_data.indexOf(';')));
                 var shopinfo_req = await axios.default.post('http://item.gmarket.co.kr/Shop/ShopInfo', JSON.parse(substring_data.substring(0, substring_data.indexOf(';'))));
                 const $shopinfo = cheerio.load(shopinfo_req.data);
@@ -139,29 +174,51 @@ getHtml(URL).then(html => {
                     }
                 });
 
-                if(data === null) {
+                if (data === null) {
                     return null;
-                }
-                else {
-                    return {
-                        '순위': i - ext_idx + 1,
-                        '상호명': data.name,
-                        '온라인매장 주소': url,
-                        '대표자명': data.ceo,
-                        '전화번호': data.tel,
-                        '응대가능시간': data.worktime,
-                        '팩스번호': data.fax,
-                        '이메일': data.email,
-                        '소재지': data.addr
+                } else {
+                    if(MODE === 'default') {
+                        return {
+                            '순번': i - ext_idx + 1,
+                            '상호명': data.name,
+                            '온라인매장 주소': url,
+                            '대표자명': data.ceo,
+                            '전화번호': data.tel,
+                            '응대가능시간': data.worktime,
+                            '팩스번호': data.fax,
+                            '이메일': data.email,
+                            '소재지': data.addr
+                        }
+                    }
+                    else {
+                        return {
+                            '순위': i - ext_idx + 1,
+                            '상호명': data.name,
+                            '온라인매장 주소': url,
+                            '대표자명': data.ceo,
+                            '전화번호': data.tel,
+                            '응대가능시간': data.worktime,
+                            '팩스번호': data.fax,
+                            '이메일': data.email,
+                            '소재지': data.addr
+                        }
                     }
                 }
             }));
+            for(var j = 0; j < seller_list.length-1; j++) {
+                if(seller_list[j].상호명 === seller_list[seller_list.length-1].상호명) {
+                    seller_list.pop();
+                    ext_idx += 1;
+                    break;
+                }
+            }
         }
 
         return seller_list.filter(n => n);
     })
     .then(seller_list => {
         const date = new Date().toString();
-        makeFolder('./data');
-        fs.writeFileSync('./data/shoplab_' + date + '.json', JSON.stringify(seller_list, null, 4));
-    })
+        makeFolder(OUTPUTDIR);
+        fs.writeFileSync(`${OUTPUTDIR}/${FILENAME}_${MODE}${SELLERS}_${date}.json`, JSON.stringify(seller_list, null, 4));
+        console.log('complete crawling');
+    });
